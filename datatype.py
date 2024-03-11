379e6c69a31571948265893914d79ede8822f9c8
kwooo5 <03230105.gcbs@rub.edu.bt>
2024-03-11 12:50:41 +0600

    first commit

---
+++ datatype.py
@@ -0,0 +1,22 @@
+x = 'hello world'# string 
+x = 50 #intenger 
+x - 60.5#float  
+x = 3j 
+x = ['greeks', 'for', 'greeks']
+x = ['greeks', 'for', 'greeks']
+x = range(10)
+x = {'name': 'suraj', 'age': 24}
+x = { 'greeks', 'for', 'greeks'}
+x = frozenset({'greeks','for', 'greek'})
+
+
+
+my_list = [1,2,3,4,5,"string"]
+my_list.insert(1,5)# insert 5 at specific at index 1
+print(my_list)
+my_list.append(5)
+print(my_list)
+print(len(my_list))
+my_list.remove(2)
+my_list.pop()
+print('list after pop', my_list)
